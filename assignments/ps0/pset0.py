ef33ffcb804240d35260743b305304779886aeea
Phil Lembo <phil.lembo@gmail.com>
2019-11-01 18:03:13 -0400

    Better py3 style formatting

--- assignments/ps0/pset0.py
+++ assignments/ps0/pset0.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov  1 17:44:32 2019
-@author: philip
+@author: Phil Lembo
 
 Write a program that does the following in order:
 1. Asks the user to enter a number “x”
@@ -14,5 +14,5 @@ import numpy as np
 
 x = int(input('Enter number for x: '))
 y = int(input('Enter number for y: '))
-print('x**y = ', x**y)
-print('log(x) = ', np.log(x))
+print('x**y = {}'.format(x**y))
+print('log(x) = {}'.format(np.log(x)))
