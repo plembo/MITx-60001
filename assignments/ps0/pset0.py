dcd79d39f1d641b0170eaf22859eae95b04020d9
Phil Lembo <phil.lembo@gmail.com>
2019-11-01 17:48:03 -0400

    Pset 0 script

---
+++ assignments/ps0/pset0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  1 17:44:32 2019
+@author: philip
+
+Write a program that does the following in order:
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”.
+"""
+import numpy as np
+
+x = int(input('Enter number for x: '))
+y = int(input('Enter number for y: '))
+print('x**y = ', x**y)
+print('log(x) = ', np.log(x))
